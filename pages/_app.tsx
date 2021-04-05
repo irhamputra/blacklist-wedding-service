deb21ef5fd7c3cf0ae6268493aa31706c6979d5f
Muhamad Irham Prasetyo <muhamad.prasetyo@get-ag.com>
2021-04-05 22:06:10 +0200

    feat: next-seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import * as React from "react";
 import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { DefaultSeo } from "next-seo";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <title>Blacklist Wedding Service in Indonesia</title>
-      </Head>
+      <DefaultSeo
+        title="Blacklist Wedding Service in Indonesia"
+        description="Kumpulan list para penipu Wedding Organizer beserta service pernikahan lainnya di Indonesia"
+        openGraph={{
+          type: "website",
+          locale: "id_ID",
+          url: "https://www.blacklist-wedding-service.com/",
+          site_name: "Blacklist Wedding Service",
+        }}
+        twitter={{
+          handle: "@irhmptra",
+          site: "@irhmptra",
+          cardType: "summary_large_image",
+        }}
+      />
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
