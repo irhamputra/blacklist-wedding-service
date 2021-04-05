f9d3bd702cc2789a2f526173806b89ed6608541a
Muhamad Irham Prasetyo <muhamad.prasetyo@get-ag.com>
2021-04-05 23:13:16 +0200

    fix: remove footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -210,14 +210,6 @@ const Home: NextPage<{
           </div>
         </div>
       </div>
-
-      <footer className="footer text-center mt-auto py-2 bg-light fixed-bottom">
-        <div className="container-fluid d-flex justify-content-center">
-          <span className="text-muted small">
-            Built with ❤️ &nbsp; for better 🇮🇩
-          </span>
-        </div>
-      </footer>
     </>
   );
 };
