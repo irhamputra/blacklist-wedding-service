4db08088e00c03f22c8928ab2f6104928112139c
Muhamad Irham Prasetyo <muhamad.prasetyo@get-ag.com>
2021-04-05 21:13:04 +0200

    fix: fixing base_url

--- pages/index.tsx
+++ pages/index.tsx
@@ -213,7 +213,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const BASE_URL =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://blacklist-wedding-services";
+      : "https://blacklist-wedding-service.com";
 
   const { data } = await axios.get(`${BASE_URL}/api/v1/user`, {
     headers: {
